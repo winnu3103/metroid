248d801723cc2a4071fc7a9cd340fded34e5054c
winnu3103 <135350109+winnu3103@users.noreply.github.com>
2023-07-18 13:00:40 -0400

    Update Publish Layer.sql

--- Publish Layer.sql
+++ Publish Layer.sql
@@ -19,3 +19,25 @@ LEFT JOIN other_tbl t2 ON DATE_TRUNC('YEAR', t1.year) - INTERVAL '1 YEAR' = DATE
 ORDER BY t1.year;
 
 select * from publish_tbl;
+
+--trend analysis for the 'recclass' for each 'year'
+SELECT year, recclass, COUNT(*) as count
+FROM publish_tbl
+GROUP BY year, recclass
+ORDER BY year;
+
+--trend analysis with % growth of 'mass' for each year
+WITH yearly_mass AS (
+  SELECT year, SUM(mass) as total_mass
+  FROM publish_tbl
+  GROUP BY year
+)
+SELECT 
+  current_year.year, 
+  current_year.total_mass as current_year_mass, 
+  previous_year.total_mass as previous_year_mass, 
+  (current_year.total_mass - previous_year.total_mass) / previous_year.total_mass * 100 as mass_growth_percentage
+FROM yearly_mass current_year
+LEFT JOIN yearly_mass previous_year ON current_year.year - INTERVAL '1 YEAR' = previous_year.year
+ORDER BY current_year.year;
+
